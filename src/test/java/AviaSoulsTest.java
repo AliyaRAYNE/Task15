fecbda09afb8a82ddbcb06f975ff7fc9c1bb63ac
Moon <Aliya0609@mail.ru>
2023-05-02 22:35:35 +0500

    Change tests second version added

--- src/test/java/AviaSoulsTest.java
+++ src/test/java/AviaSoulsTest.java
@@ -56,6 +56,12 @@ public class AviaSoulsTest {
     @Test
     public void SortingAndSearchingByTicketPrice() {
         AviaSouls souls = new AviaSouls();
+        Ticket ticket_1 = new Ticket("Уфа", "Казань", 8450, 10, 12);
+        Ticket ticket_2 = new Ticket("Уфа", "Казань", 4700, 10, 22);
+        Ticket ticket_3 = new Ticket("Уфа", "Казань", 4700, 10, 16);
+        Ticket ticket_4 = new Ticket("Уфа", "Казань", 16000, 10, 18);
+        Ticket ticket_5 = new Ticket("Уфа", "Казань", 5500, 10, 20);
+        Ticket ticket_6 = new Ticket("Уфа", "Казань", 9600, 10, 14);
         souls.add(ticket_1);
         souls.add(ticket_2);
         souls.add(ticket_3);
@@ -63,11 +69,7 @@ public class AviaSoulsTest {
         souls.add(ticket_5);
         souls.add(ticket_6);
 
-        TicketTimeComparator timeComparator = new TicketTimeComparator();
-
-        Ticket[] tickets = {ticket_1, ticket_2, ticket_3, ticket_4, ticket_5, ticket_6};
-        Arrays.sort(tickets, timeComparator);
-
+        Ticket[] tickets = souls.search("Уфа","Казань");
         Ticket[] expected = {ticket_2, ticket_3, ticket_5, ticket_1, ticket_6, ticket_4};
 
         Assertions.assertArrayEquals(expected, tickets);
@@ -76,6 +78,12 @@ public class AviaSoulsTest {
     @Test
     public void SortingAndSearchingByFlightTime() {
         AviaSouls souls = new AviaSouls();
+        Ticket ticket_1 = new Ticket("Уфа", "Казань", 8450, 10, 12);
+        Ticket ticket_2 = new Ticket("Уфа", "Казань", 4700, 10, 22);
+        Ticket ticket_3 = new Ticket("Уфа", "Казань", 4700, 10, 16);
+        Ticket ticket_4 = new Ticket("Уфа", "Казань", 16000, 10, 18);
+        Ticket ticket_5 = new Ticket("Уфа", "Казань", 5500, 10, 20);
+        Ticket ticket_6 = new Ticket("Уфа", "Казань", 9600, 10, 14);
         souls.add(ticket_1);
         souls.add(ticket_2);
         souls.add(ticket_3);
@@ -85,9 +93,7 @@ public class AviaSoulsTest {
 
         TicketTimeComparator timeComparator = new TicketTimeComparator();
 
-        Ticket[] tickets = {ticket_1, ticket_2, ticket_3, ticket_4, ticket_5, ticket_6};
-        Arrays.sort(tickets, timeComparator);
-
+        Ticket[] tickets = souls.searchAndSortBy("Уфа","Казань", timeComparator);
         Ticket[] expected = {ticket_1, ticket_6, ticket_3, ticket_4, ticket_5, ticket_2};
 
         Assertions.assertArrayEquals(expected, tickets);
